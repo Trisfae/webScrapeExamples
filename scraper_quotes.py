a80909eaf5d51844bc7258a8cab8b3730a10be42
Martin <trisfae@gmail.com>
2020-04-11 12:32:39 -0700

    this is the first and only commit for this

---
+++ scraper_quotes.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+
+counter = 1
+
+for x in range(10):
+    link = ('http://quotes.toscrape.com/page/' + str(counter) + '/')
+    print ('Scraping: ' + link)
+    source = requests.get(link).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    for div in soup.find_all('div', class_='quote'):
+
+        # empty print to line break for loop list
+        print()
+
+        # prints out the quote
+        quote_text = div.span.text
+        print(quote_text)
+
+        # prints out the author of the quote
+        quote_author = div.small.text
+        print ('    ' + '- ' +  quote_author)
+
+        # prints out the tags for the quote in csv format
+        tags = div.find_all('a', class_='tag')
+        print ('        ' +'TAGS')
+        for text in tags:
+            print('            ' + text.text)
+
+    counter += 1
+counter = 0
+
+
+
+
+
+
+
